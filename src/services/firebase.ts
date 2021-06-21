b320a3eaa1c5aa3ec60f53874e6fb55f982b521e
Vinicus <viniciusalcantarabarros@gmail.com>
2021-06-21 18:04:33 -0300

    realizando conexão REACT with Firebase

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -4,13 +4,13 @@ import "firebase/auth";
 import "firebase/database";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCh5J9yFK_9oHobJSHAHer-qi8CWVz6jao",
-    authDomain: "letmesask.firebaseapp.com",
-    databaseURL: "https://letmesask-default-rtdb.firebaseio.com",
-    projectId: "letmesask",
-    storageBucket: "letmesask.appspot.com",
-    messagingSenderId: "534718043981",
-    appId: "1:534718043981:web:7690417a695b912a984904"
+    apiKey: process.env.REACT_APP_API_KEY,
+    authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+    databaseURL: process.env.REACT_APP_DATABASE_URL,
+    projectId: process.env.REACT_APP_PROJECT_ID,
+    storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+    messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+    appId: process.env.REACT_APP_APP_ID
 };
 
 firebase.initializeApp(firebaseConfig)
