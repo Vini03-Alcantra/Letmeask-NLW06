f5c0d7e396e2ba65300d113a761fa082cd142072
Vinicus <viniciusalcantarabarros@gmail.com>
2021-06-25 22:44:00 -0300

    Removendo pergutna

--- src/pages/AdminRoom.tsx
+++ src/pages/AdminRoom.tsx
@@ -1,13 +1,14 @@
 import {useParams} from "react-router-dom";
-import logoImg from "../assets/images/logo.svg"
 import { Button } from "../components/Button"
 import { Question } from "../components/Question";
 import { RoomCode } from "../components/RoomCode"
 import { useRoom } from "../hooks/useRoom";
 
-
+import logoImg from "../assets/images/logo.svg"
+import deleteImg from "../assets/images/delete.svg"
 
 import '../styles/room.scss'
+import { database } from "../services/firebase";
 
 
 
@@ -19,6 +20,12 @@ export function AdminRoom(){
     const params = useParams<RoomParams>()
     const roomId = params.id
     const {title, questions} = useRoom(roomId)
+
+    async function handleDeleteQuestion(questionId: string){
+        if (window.confirm("you have sure that remove this question")) {
+            await database.ref(`rooms/${roomId}/questions/${questionId}`).remove()
+        }
+    }
 
     return (  
         <div id="page-room">
@@ -45,7 +52,13 @@ export function AdminRoom(){
                                 key={question.id}
                                 content={question.content}
                                 author={question.author}
-                            />
+                            >
+
+                            <button type="submit" onClick={() => handleDeleteQuestion(question.id)}>
+                                <img src={deleteImg} alt="Remover pergunta" />
+                            </button>
+
+                            </Question>
                         )
                     })}
                 </div>
